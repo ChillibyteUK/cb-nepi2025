e49ad23a0fe68067394811a42b863ae9d98b6b67
ChillibyteUK <dan@chillibyte.co.uk>
2025-01-06 14:03:01 +0000

    Update cb_contact.php

--- page-templates/blocks/cb_contact.php
+++ page-templates/blocks/cb_contact.php
@@ -7,7 +7,7 @@
                     <li><span class="fa-li"><i class="fas fa-map-marker-alt fs-500"></i></span> <strong>Address:</strong><br><?= get_field('contact_address', 'option') ?></li>
                 </ul>
             </div>
-            <div class="col-md-6 col-lg-4">
+            <div class="col-md-6 col-lg-3">
                 <ul class="fa-ul">
                     <li class="mb-2"><span class="fa-li"><i class="fas fa-phone fs-500"></i></span> <strong>Phone:</strong> <?= do_shortcode('[contact_phone]') ?></li>
                     <li><span class="fa-li"><i class="fas fa-envelope fs-500"></i></span> <strong>Email:</strong> <?= do_shortcode('[contact_email]') ?></li>
