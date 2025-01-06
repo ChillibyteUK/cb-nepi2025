ef9bbc42473701b103a55e2eaebb3b1454489450
ChillibyteUK <dan@chillibyte.co.uk>
2025-01-06 13:12:19 +0000

    Update cb_youtube.php

--- page-templates/blocks/cb_youtube.php
+++ page-templates/blocks/cb_youtube.php
@@ -2,7 +2,9 @@
 $url = get_field('youtube_url');
 ?>
 <section class="youtube py-5">
-    <div class="ratio ratio-16x9">
-        <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/<?=$url?>" allowscriptaccess="always" allow="autoplay;fullscreen"></iframe>
+    <div class="container-xl">
+        <div class="ratio ratio-16x9">
+            <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/<?= $url ?>" allowscriptaccess="always" allow="autoplay;fullscreen"></iframe>
+        </div>
     </div>
 </section>
